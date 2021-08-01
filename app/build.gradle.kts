16d0cfde66986060d29b857824c6b56481840541
Mufidz <rmufidianto@outlook.com>
2021-08-01 14:55:28 +0700

    Update depedencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-parcelize")
@@ -6,19 +9,24 @@ plugins {
     id("androidx.navigation.safeargs.kotlin")
 }
 
+val localPropertiesFile = rootProject.file("local.properties")
+val localProperties = Properties()
+localProperties.load(FileInputStream(localPropertiesFile))
+val apiKey = localProperties["apiKey"]
+
 android {
-    compileSdkVersion(30)
+    compileSdk = 30
     buildToolsVersion = "30.0.3"
 
     defaultConfig {
         applicationId = "com.mufidz.nontonkuy"
-        minSdkVersion(23)
-        targetSdkVersion(30)
+        minSdk = 23
+        targetSdk = 30
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        buildConfigField("String", "MOVIEDB_APIKEY", "\"[YOUR_MOVIEDB_APIKEY]\"")
+        buildConfigField("String", "MOVIEDB_APIKEY", "$apiKey")
     }
 
     buildTypes {
@@ -44,13 +52,13 @@ android {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.5.20")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.5.21")
     implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("androidx.fragment:fragment-ktx:1.3.5")
-    implementation("io.coil-kt:coil:1.2.2")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.fragment:fragment-ktx:1.3.6")
+    implementation("io.coil-kt:coil:1.3.0")
     implementation("io.coil-kt:coil-svg:1.2.2")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
